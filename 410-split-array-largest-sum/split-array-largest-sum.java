102a2f6f67801a556d724a51062a30abcc0cc932
yash12R <yashraut584@gmail.com>
2026-07-19 16:29:04 +0530

    Time: 2 ms (30.42%) | Memory: 43.5 MB (6.83%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,63 @@
+
+
+class Solution {
+
+    boolean isPossible(int[] arr, long barrier, int k) {
+        int students = 1;
+        long pages = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > barrier)
+                return false;
+
+            if (pages + arr[i] > barrier) {
+                students++;
+
+                if (students > k)
+                    return false;
+
+                pages = arr[i];
+            } else {
+                pages += arr[i];
+            }
+        }
+
+        return true;
+    }
+
+    public int findpages(int[] arr, int k) {
+
+        int n = arr.length;
+
+        // Impossible case
+        if (k > n)
+            return -1;
+
+        long low = Arrays.stream(arr).max().getAsInt();
+
+        long high = 0;
+        for (int x : arr)
+            high += x;
+
+        long ans = -1;
+
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+
+            if (isPossible(arr, mid, k)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return (int) ans;
+    }
+
+    public int splitArray(int[] nums, int k) {
+        int n=nums.length;
+      return findpages(nums,k);  
+    }
+}
